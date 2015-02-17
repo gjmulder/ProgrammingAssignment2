dae7f8a25104b591bc1536440da9e988cedb39b6
Gary Mulder <gjmulder@gmail.com>
2015-02-17 16:12:30 +0000

    Added functions for cacheable matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A cacheable matrix mini-library
 
-## Write a short comment describing this function
+# Example usage:
+#
+# > m <- matrix(1:4, nrow=2, ncol=2)
+# > cm <- makeCacheMatrix(m)
+# > cacheSolve(cm)
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > cacheSolve(cm)
+# getting cached data
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
 
+# Create a cacheable matrix with associated functions to access it
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv) 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Return the inverse of the cacheable matrix
+# If the inverse has been cached return the cached matrix
+cacheSolve <- function(x) {
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinv(i)
+  i
 }
